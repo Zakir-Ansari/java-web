4dc4553d0fbf4d02a251608c854181177d5814c6
Zakir-Ansari <azakir780@gmail.com>
2019-09-09 20:21:30 +0530

    Initial Commit

---
+++ CC_Eligibility_Validator/src/main/java/com/zakir/wipro/bean/PAN.java
@@ -0,0 +1,17 @@
+package com.zakir.wipro.bean;
+
+public class PAN {
+	private String panNumber;
+
+	public PAN() {
+		
+	}
+	
+	public String getPanNumber() {
+		return panNumber;
+	}
+
+	public void setPanNumber(String panNumber) {
+		this.panNumber = panNumber;
+	}
+}
